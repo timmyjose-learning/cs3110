ca1d03bedfc23383d354f728338dcd3951433b17
Timmy Jose <zoltan.jose@gmail.com>
2026-04-17 09:48:20 +0530

    feat: ch10 - promises as a write-once data structure

--- ch10/promises/lib/promises.ml
+++ ch10/promises/lib/promises.ml
@@ -1,15 +1,27 @@
-(* Lwt style custom implementation *)
-
+(** Simulating Lwt's promise types *)
 module type PROMISE = sig
   type 'a state = Pending | Fulfilled of 'a | Rejected of exn
   type 'a promise
   type 'a resolver
 
   val make : unit -> 'a promise * 'a resolver
+  (** [make ()] is a new promise and a new resolver. The promise is pending *)
+
   val return : 'a -> 'a promise
+  (** [return x] is a new promise that is already resolved with value [x] *)
+
   val state : 'a promise -> 'a state
+  (** [state p] is the state of the promise p *)
+
   val fulfill : 'a resolver -> 'a -> unit
+  (** [fulfill r x] fulfills the promise associated with the resolver [r] with
+      value [x], meaning that [state p] will return [Fulfilled x]. Requires: [p]
+      is pending *)
+
   val reject : 'a resolver -> exn -> unit
+  (** [reject r e] rejects the promise associated with the resolver [r] with
+      exception [e], meaning that [state p] will return [Rejected e]. Requires:
+      [p] is pending *)
 end
 
 module Promise : PROMISE = struct
@@ -18,7 +30,7 @@ module Promise : PROMISE = struct
   type 'a resolver = 'a promise
 
   let write_once p s =
-    if !p = Pending then p := s else invalid_arg "cannot write twice"
+    if !p = Pending then p := s else invalid_arg "cannot write more than once"
 
   let make () =
     let p = ref Pending in
